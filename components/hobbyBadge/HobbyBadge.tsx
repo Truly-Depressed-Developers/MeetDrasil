68d5781942569787046608a3acd6a66ed37daf29
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2025-04-12 23:21:31 +0200

    Add events list

--- components/hobbyBadge/HobbyBadge.tsx
+++ components/hobbyBadge/HobbyBadge.tsx
@@ -1,17 +1,29 @@
 import { Badge } from '@/components/ui/badge';
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
 
 type BadgeWithImageProps = {
+  className?: string;
   name: string;
+  iconSize?: number;
   imageUrl: string;
   isActive: boolean;
 };
 
-const BadgeWithImage = ({ name, imageUrl, isActive }: BadgeWithImageProps) => {
+const BadgeWithImage = ({ className, name, iconSize, imageUrl, isActive }: BadgeWithImageProps) => {
   return (
-    <Badge className="rounded-full px-3 py-2" variant={isActive ? 'default' : 'outline'}>
-      <Image src={imageUrl} className="mr-2 size-5 rounded-full" alt="" height={20} width={20} />
-      <span className="text-base">{name}</span>
+    <Badge
+      className={cn('rounded-full px-3 py-2 text-base', className)}
+      variant={isActive ? 'default' : 'outline'}
+    >
+      <Image
+        src={imageUrl}
+        className="mr-2 rounded-full"
+        alt="icon"
+        height={iconSize || 20}
+        width={iconSize || 20}
+      />
+      {name}
     </Badge>
   );
 };
